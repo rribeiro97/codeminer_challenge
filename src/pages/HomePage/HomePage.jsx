da7d7011f61d3a47141b723ffd6d56b06cc0e762
Ricardo <rcordeiro.ricardo@gmail.com>
2020-07-12 03:08:18 -0300

    Choosing the folder pattern  and adding homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, {useState,useEffect} from 'react';
+import axios from 'axios';
 import CartContainer from '../../containers/CartContainer/CartContainer'
 import ProductContainer from '../../containers/ProductContainer/ProductContainer'
 import './HomePage.scss';
@@ -7,26 +8,42 @@ const HomePage = () => {
     const [fetchedProducts,setFetchedProducts] = useState([]);
     const [fetchedVouchers,setFetchedVouchers] = useState([]);
 
-
     useEffect(() =>{
-        fetch(`https://shielded-wildwood-82973.herokuapp.com/products.json`)
-        .then((res) => res.json())
-        .then(data => {
-          setFetchedProducts(data);
-          console.log('products',data);
-        });
-
-        fetch(`https://shielded-wildwood-82973.herokuapp.com/vouchers.json`)
-        .then((res) => res.json())
-        .then(data => {
-          setFetchedVouchers(data);
-          console.log(data);
-        });
+        fetchProducts();
+        fetchVouchers();
     },[])
+
+    const fetchProducts = async () => {
+        axios.get(`https://shielded-wildwood-82973.herokuapp.com/products.json`)
+        .then( res => {
+            console.log('axiosres', res);
+            setFetchedProducts(res.data.products);
+        }).catch( error => {
+            console.log('prod error',error);
+            fetchProducts();
+        });
+    }
+    
+    const fetchVouchers = async () => {
+        axios.get(`https://shielded-wildwood-82973.herokuapp.com/vouchers.json`)
+        .then( res => {
+            console.log('axiosresVoucher', res);
+            setFetchedVouchers(res.data.vouchers);
+        }).catch( error => {
+            console.log('voucher',error);
+            fetchVouchers();
+        });
+    }
+   
+console.log('fetchProd',fetchedProducts);
+console.log('fetchVouchers',fetchedVouchers);
+
     return (
         <div className="HomePage container">
-            <ProductContainer products={fetchedProducts}/>
-            <CartContainer />
+                { fetchedProducts && 
+                    <ProductContainer products={fetchedProducts}/>
+                }  
+            <CartContainer selectedProducts={fetchedProducts}/>
         </div>
     );
 };
